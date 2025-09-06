a43a22db5559d35ecf9c241ed1301f4ea4623ec6
Agent026 <23501a4226@pvpsit.ac.in>
2025-09-06 09:49:20 +0530

    Update vite-env.d.ts
    Signed-off-by: Agent026 <23501a4226@pvpsit.ac.in>

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/mail-mojo-ai/", // 👈 change this to your repo name
+  plugins: [react()],
+});
 /// <reference types="vite/client" />
